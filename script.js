609a71277d3254b5b4c7eb0ebd088515dcd9afee
coolrunner1 <153802775+coolrunner1@users.noreply.github.com>
2024-04-29 16:46:12 +0300

    reworked theme functionality

--- script.js
+++ script.js
@@ -4,17 +4,17 @@ divImg.style.width="100%";
 divImg.style.height="100%";
 let themeName=getComputedStyle(document.body).getPropertyValue('--theme-name');
 let root=document.querySelector(':root');
-let themeMode;
 
-console.log(themeName)
-if (themeName=='"light"'){
-    divImg.style.backgroundImage = "url('./images/sun-svgrepo-com.svg')";
-    themeMode=0;
-}
 
-else {
+if (themeName=='"dark"') {
+    root.className='dark';
     divImg.style.backgroundImage = "url('./images/crescent-moon-svgrepo-com.svg')";
     themeMode=1;
+}
+else {
+    root.className='light';
+    divImg.style.backgroundImage = "url('./images/sun-svgrepo-com.svg')";
+    themeMode=0;
 }
 
 divImg.style.backgroundRepeat= "no-repeat";
@@ -23,8 +23,9 @@ themebtn.appendChild(divImg);
 themebtn.onclick = () => {
     if (themeMode){
         themeMode=0;
+        root.className='light';
         document.getElementById("header").style.backgroundColor = "#ffffff";
-        document.getElementById("right-container").style.backgroundColor = "#e2e8f0";
+        //document.getElementById("right-container").style.backgroundColor = "#e2e8f0";
         document.body.style.color = "black";
         root.style.setProperty('--sidebar-color','#000000');
         root.style.setProperty('--svg-fill','#000000');
@@ -32,11 +33,13 @@ themebtn.onclick = () => {
     }
     else {
         themeMode=1;
-        document.getElementById("header").style.backgroundColor = "#181818";
+        root.className='dark';
+        /*document.getElementById("header").style.backgroundColor = "#181818";
         document.getElementById("right-container").style.backgroundColor = "#232327";
         document.body.style.color = "white";
         root.style.setProperty('--sidebar-color','#ffffff');
-        root.style.setProperty('--svg-fill','#ffffff');
+        root.style.setProperty('--svg-fill','#ffffff');*/
         divImg.style.backgroundImage = "url('./images/crescent-moon-svgrepo-com.svg')";
     }
 }
+
