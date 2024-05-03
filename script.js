68faddacfbebd711b37d8ba70c8486ba2f43835c
coolrunner1 <153802775+coolrunner1@users.noreply.github.com>
2024-05-03 20:26:01 +0300

    Added a second row

--- script.js
+++ script.js
@@ -20,21 +20,11 @@ themebtn.onclick = () => {
     if (themeMode){
         themeMode=0;
         root.className='light';
-        //document.getElementById("header").style.backgroundColor = "#ffffff";
-        //document.getElementById("right-container").style.backgroundColor = "#e2e8f0";
-        //document.body.style.color = "black";
-        //root.style.setProperty('--sidebar-color','#000000');
-        //root.style.setProperty('--svg-fill','#000000');
         divImg.style.backgroundImage = "url('./images/sun-svgrepo-com.svg')";
     }
     else {
         themeMode=1;
         root.className='dark';
-        /*document.getElementById("header").style.backgroundColor = "#181818";
-        document.getElementById("right-container").style.backgroundColor = "#232327";
-        document.body.style.color = "white";
-        root.style.setProperty('--sidebar-color','#ffffff');
-        root.style.setProperty('--svg-fill','#ffffff');*/
         divImg.style.backgroundImage = "url('./images/crescent-moon-svgrepo-com.svg')";
     }
 }
